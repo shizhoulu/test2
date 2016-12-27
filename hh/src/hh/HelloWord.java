f2f79578d5e3c839fd722c1ce6683a07fcb57265
dashi <shizhoulu_1@126.com>
2016-12-27 18:27:11 +0800

    去掉电脑

--- hh/src/hh/HelloWord.java
+++ hh/src/hh/HelloWord.java
@@ -2,6 +2,6 @@ package hh;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("hello word 33333333366666777788889999910000000");
+		System.out.println("hello word");
 	}
 }
