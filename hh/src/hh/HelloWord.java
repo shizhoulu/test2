b96bee1402b95a80781d8a507a10ba197d3d0fed
shizhoulu <shizhoulu_1@126.com>
2016-12-27 18:17:12 +0800

    66666

--- hh/src/hh/HelloWord.java
+++ hh/src/hh/HelloWord.java
@@ -2,6 +2,6 @@ package hh;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("hello word 333333333");
+		System.out.println("hello word 33333333366666");
 	}
 }
