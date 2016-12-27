18941c5b783823fe95fb3a990f4b2f1f851d5cbe
dashi <shizhoulu_1@126.com>
2016-12-27 18:19:36 +0800

    77777

--- hh/src/hh/HelloWord.java
+++ hh/src/hh/HelloWord.java
@@ -2,6 +2,6 @@ package hh;
 
 public class HelloWord {
 	public static void main(String[] args) {
-		System.out.println("hello word 33333333366666");
+		System.out.println("hello word 333333333666667777");
 	}
 }
